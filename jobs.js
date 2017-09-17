61811cfc781778cddb1007c4630b5067ae439705
snghnaveen <sngh.naveen@gmail.com>
2017-09-17 20:17:04 +0530

    running jobs fast, for debugging

--- jobs.js
+++ jobs.js
@@ -7,7 +7,7 @@ var sqlite3 = require('sqlite3').verbose(),
     service = require('./app/services/'),
     chalk = require('chalk');
 
-schedule.scheduleJob('17 * * * *', function () {
+schedule.scheduleJob('*/20 * * * * *', function () {
     console.log(chalk.bgWhite.black('Job Running...' + Date()));
 
     waterfall([
